6eee73db0fc6fc1656169e989fab5489c881f7e2
x81k25 <x81k25@gmail.com>
2025-04-02 11:28:10 -0500

    adding pgadmin4

--- modules/pgsql/main.tf
+++ modules/pgsql/main.tf
@@ -12,9 +12,77 @@ resource "kubernetes_namespace" "pgsql" {
 }
 
 ################################################################################
-# PostgreSQL Dev Environment Secrets
+# Flattened ConfigMaps for each environment
 ################################################################################
 
+# Development Environment ConfigMap
+resource "kubernetes_config_map" "pgsql_dev_config" {
+  metadata {
+    name      = "pgsql-dev-config"
+    namespace = kubernetes_namespace.pgsql.metadata[0].name
+    labels = {
+      app        = "pgsql"
+      environment = "dev"
+      managed-by = "terraform"
+    }
+  }
+
+  data = {
+    pgsql_dev_user     = var.pgsql_config.dev.user
+    pgsql_dev_database = var.pgsql_config.dev.database
+    pgsql_dev_port     = tostring(var.pgsql_config.dev.port)
+    pgsql_dev_mount    = var.pgsql_config.dev.mount
+    server_ip          = var.server_ip
+  }
+}
+
+# Staging Environment ConfigMap
+resource "kubernetes_config_map" "pgsql_stg_config" {
+  metadata {
+    name      = "pgsql-stg-config"
+    namespace = kubernetes_namespace.pgsql.metadata[0].name
+    labels = {
+      app        = "pgsql"
+      environment = "stg"
+      managed-by = "terraform"
+    }
+  }
+
+  data = {
+    pgsql_stg_user     = var.pgsql_config.stg.user
+    pgsql_stg_database = var.pgsql_config.stg.database
+    pgsql_stg_port     = tostring(var.pgsql_config.stg.port)
+    pgsql_stg_mount    = var.pgsql_config.stg.mount
+    server_ip          = var.server_ip
+  }
+}
+
+# Production Environment ConfigMap
+resource "kubernetes_config_map" "pgsql_prod_config" {
+  metadata {
+    name      = "pgsql-prod-config"
+    namespace = kubernetes_namespace.pgsql.metadata[0].name
+    labels = {
+      app        = "pgsql"
+      environment = "prod"
+      managed-by = "terraform"
+    }
+  }
+
+  data = {
+    pgsql_prod_user     = var.pgsql_config.prod.user
+    pgsql_prod_database = var.pgsql_config.prod.database
+    pgsql_prod_port     = tostring(var.pgsql_config.prod.port)
+    pgsql_prod_mount    = var.pgsql_config.prod.mount
+    server_ip           = var.server_ip
+  }
+}
+
+################################################################################
+# Flattened Secrets for each environment
+################################################################################
+
+# Development Environment Secrets
 resource "kubernetes_secret" "pgsql_dev_secrets" {
   metadata {
     name      = "pgsql-dev-secrets"
@@ -27,21 +95,18 @@ resource "kubernetes_secret" "pgsql_dev_secrets" {
   }
 
   data = {
-    username = var.pgsql_config.dev.user
-    password = var.pgsql_config.dev.password
-    database = var.pgsql_config.dev.database
-    port     = tostring(var.pgsql_config.dev.port)
-    server_ip = var.server_ip
-    mount    = var.pgsql_config.dev.mount
+    pgsql_dev_user     = var.pgsql_config.dev.user
+    pgsql_dev_password = var.pgsql_config.dev.password
+    pgsql_dev_database = var.pgsql_config.dev.database
+    pgsql_dev_port     = tostring(var.pgsql_config.dev.port)
+    server_ip          = var.server_ip
+    pgsql_dev_mount    = var.pgsql_config.dev.mount
   }
 
   type = "Opaque"
 }
 
-################################################################################
-# PostgreSQL Staging Environment Secrets
-################################################################################
-
+# Staging Environment Secrets
 resource "kubernetes_secret" "pgsql_stg_secrets" {
   metadata {
     name      = "pgsql-stg-secrets"
@@ -54,21 +119,18 @@ resource "kubernetes_secret" "pgsql_stg_secrets" {
   }
 
   data = {
-    username = var.pgsql_config.stg.user
-    password = var.pgsql_config.stg.password
-    database = var.pgsql_config.stg.database
-    port     = tostring(var.pgsql_config.stg.port)
-    server_ip = var.server_ip
-    mount    = var.pgsql_config.stg.mount
+    pgsql_stg_user     = var.pgsql_config.stg.user
+    pgsql_stg_password = var.pgsql_config.stg.password
+    pgsql_stg_database = var.pgsql_config.stg.database
+    pgsql_stg_port     = tostring(var.pgsql_config.stg.port)
+    server_ip          = var.server_ip
+    pgsql_stg_mount    = var.pgsql_config.stg.mount
   }
 
   type = "Opaque"
 }
 
-################################################################################
-# PostgreSQL Production Environment Secrets
-################################################################################
-
+# Production Environment Secrets
 resource "kubernetes_secret" "pgsql_prod_secrets" {
   metadata {
     name      = "pgsql-prod-secrets"
@@ -81,22 +143,23 @@ resource "kubernetes_secret" "pgsql_prod_secrets" {
   }
 
   data = {
-    username = var.pgsql_config.prod.user
-    password = var.pgsql_config.prod.password
-    database = var.pgsql_config.prod.database
-    port     = tostring(var.pgsql_config.prod.port)
-    server_ip = var.server_ip
-    mount    = var.pgsql_config.prod.mount
+    pgsql_prod_user     = var.pgsql_config.prod.user
+    pgsql_prod_password = var.pgsql_config.prod.password
+    pgsql_prod_database = var.pgsql_config.prod.database
+    pgsql_prod_port     = tostring(var.pgsql_config.prod.port)
+    server_ip           = var.server_ip
+    pgsql_prod_mount    = var.pgsql_config.prod.mount
   }
 
   type = "Opaque"
 }
 
 ################################################################################
-# PostgreSQL Performance Config Secrets
+# Flattened Performance ConfigMaps
 ################################################################################
 
-resource "kubernetes_secret" "pgsql_dev_performance" {
+# Development Performance ConfigMap
+resource "kubernetes_config_map" "pgsql_dev_performance" {
   metadata {
     name      = "pgsql-dev-performance"
     namespace = kubernetes_namespace.pgsql.metadata[0].name
@@ -109,17 +172,16 @@ resource "kubernetes_secret" "pgsql_dev_performance" {
   }
 
   data = {
-    shared_buffers      = tostring(var.pgsql_config.dev.performance.shared_buffers)
-    work_mem            = tostring(var.pgsql_config.dev.performance.work_mem)
-    maintenance_work_mem = tostring(var.pgsql_config.dev.performance.maintenance_work_mem)
-    max_connections     = tostring(var.pgsql_config.dev.performance.max_connections)
-    effective_cache_size = tostring(var.pgsql_config.dev.performance.effective_cache_size)
+    pgsql_dev_shared_buffers      = tostring(var.pgsql_config.dev.performance.shared_buffers)
+    pgsql_dev_work_mem            = tostring(var.pgsql_config.dev.performance.work_mem)
+    pgsql_dev_maintenance_work_mem = tostring(var.pgsql_config.dev.performance.maintenance_work_mem)
+    pgsql_dev_max_connections     = tostring(var.pgsql_config.dev.performance.max_connections)
+    pgsql_dev_effective_cache_size = tostring(var.pgsql_config.dev.performance.effective_cache_size)
   }
-
-  type = "Opaque"
 }
 
-resource "kubernetes_secret" "pgsql_stg_performance" {
+# Staging Performance ConfigMap
+resource "kubernetes_config_map" "pgsql_stg_performance" {
   metadata {
     name      = "pgsql-stg-performance"
     namespace = kubernetes_namespace.pgsql.metadata[0].name
@@ -132,17 +194,16 @@ resource "kubernetes_secret" "pgsql_stg_performance" {
   }
 
   data = {
-    shared_buffers      = tostring(var.pgsql_config.stg.performance.shared_buffers)
-    work_mem            = tostring(var.pgsql_config.stg.performance.work_mem)
-    maintenance_work_mem = tostring(var.pgsql_config.stg.performance.maintenance_work_mem)
-    max_connections     = tostring(var.pgsql_config.stg.performance.max_connections)
-    effective_cache_size = tostring(var.pgsql_config.stg.performance.effective_cache_size)
+    pgsql_stg_shared_buffers      = tostring(var.pgsql_config.stg.performance.shared_buffers)
+    pgsql_stg_work_mem            = tostring(var.pgsql_config.stg.performance.work_mem)
+    pgsql_stg_maintenance_work_mem = tostring(var.pgsql_config.stg.performance.maintenance_work_mem)
+    pgsql_stg_max_connections     = tostring(var.pgsql_config.stg.performance.max_connections)
+    pgsql_stg_effective_cache_size = tostring(var.pgsql_config.stg.performance.effective_cache_size)
   }
-
-  type = "Opaque"
 }
 
-resource "kubernetes_secret" "pgsql_prod_performance" {
+# Production Performance ConfigMap
+resource "kubernetes_config_map" "pgsql_prod_performance" {
   metadata {
     name      = "pgsql-prod-performance"
     namespace = kubernetes_namespace.pgsql.metadata[0].name
@@ -155,44 +216,101 @@ resource "kubernetes_secret" "pgsql_prod_performance" {
   }
 
   data = {
-    shared_buffers      = tostring(var.pgsql_config.prod.performance.shared_buffers)
-    work_mem            = tostring(var.pgsql_config.prod.performance.work_mem)
-    maintenance_work_mem = tostring(var.pgsql_config.prod.performance.maintenance_work_mem)
-    max_connections     = tostring(var.pgsql_config.prod.performance.max_connections)
-    effective_cache_size = tostring(var.pgsql_config.prod.performance.effective_cache_size)
+    pgsql_prod_shared_buffers      = tostring(var.pgsql_config.prod.performance.shared_buffers)
+    pgsql_prod_work_mem            = tostring(var.pgsql_config.prod.performance.work_mem)
+    pgsql_prod_maintenance_work_mem = tostring(var.pgsql_config.prod.performance.maintenance_work_mem)
+    pgsql_prod_max_connections     = tostring(var.pgsql_config.prod.performance.max_connections)
+    pgsql_prod_effective_cache_size = tostring(var.pgsql_config.prod.performance.effective_cache_size)
   }
-
-  type = "Opaque"
 }
 
 ################################################################################
-# PostgreSQL Security Config Secrets
+# Flattened Security ConfigMaps
 ################################################################################
 
-resource "kubernetes_secret" "pgsql_security" {
-  for_each = {
-    dev  = var.pgsql_config.dev.security
-    stg  = var.pgsql_config.stg.security
-    prod = var.pgsql_config.prod.security
-  }
-
+# Development Security ConfigMap
+resource "kubernetes_config_map" "pgsql_dev_security" {
   metadata {
-    name      = "pgsql-${each.key}-security"
+    name      = "pgsql-dev-security"
     namespace = kubernetes_namespace.pgsql.metadata[0].name
     labels = {
       app        = "pgsql"
-      environment = each.key
+      environment = "dev"
       managed-by = "terraform"
       type       = "security"
     }
   }
 
   data = {
-    uid = tostring(each.value.UID)
-    gid = tostring(each.value.GID)
+    pgsql_dev_uid = tostring(var.pgsql_config.dev.security.UID)
+    pgsql_dev_gid = tostring(var.pgsql_config.dev.security.GID)
+  }
+}
+
+# Staging Security ConfigMap
+resource "kubernetes_config_map" "pgsql_stg_security" {
+  metadata {
+    name      = "pgsql-stg-security"
+    namespace = kubernetes_namespace.pgsql.metadata[0].name
+    labels = {
+      app        = "pgsql"
+      environment = "stg"
+      managed-by = "terraform"
+      type       = "security"
+    }
   }
 
-  type = "Opaque"
+  data = {
+    pgsql_stg_uid = tostring(var.pgsql_config.stg.security.UID)
+    pgsql_stg_gid = tostring(var.pgsql_config.stg.security.GID)
+  }
+}
+
+# Production Security ConfigMap
+resource "kubernetes_config_map" "pgsql_prod_security" {
+  metadata {
+    name      = "pgsql-prod-security"
+    namespace = kubernetes_namespace.pgsql.metadata[0].name
+    labels = {
+      app        = "pgsql"
+      environment = "prod"
+      managed-by = "terraform"
+      type       = "security"
+    }
+  }
+
+  data = {
+    pgsql_prod_uid = tostring(var.pgsql_config.prod.security.UID)
+    pgsql_prod_gid = tostring(var.pgsql_config.prod.security.GID)
+  }
+}
+
+################################################################################
+# Flattened pgAdmin4 ConfigMap
+################################################################################
+
+resource "kubernetes_config_map" "pgadmin4_config" {
+  metadata {
+    name      = "pgadmin4-config"
+    namespace = kubernetes_namespace.pgsql.metadata[0].name
+    labels = {
+      app        = "pgadmin4"
+      managed-by = "terraform"
+    }
+  }
+
+  data = {
+    pgadmin4_email         = var.pgadmin4_config.email
+    pgadmin4_uid           = tostring(var.pgadmin4_config.UID)
+    pgadmin4_gid           = tostring(var.pgadmin4_config.GID)
+    pgadmin4_fs_group      = tostring(var.pgadmin4_config.fs_group)
+    pgadmin4_listen_address = var.pgadmin4_config.listen_address
+    pgadmin4_listen_port    = tostring(var.pgadmin4_config.listen_port)
+    pgadmin4_server_mode    = tostring(var.pgadmin4_config.server_mode)
+    pgadmin4_server_ip      = var.server_ip
+    pgadmin4_mount          = var.pgadmin4_config.mount
+    pgadmin4_port           = tostring(var.pgadmin4_config.port)
+  }
 }
 
 ################################################################################
@@ -210,29 +328,29 @@ resource "kubernetes_secret" "pgadmin4_secrets" {
   }
 
   data = {
-    email         = var.pgadmin4_config.email
-    password      = var.pgadmin4_config.password
-    uid           = tostring(var.pgadmin4_config.UID)
-    gid           = tostring(var.pgadmin4_config.GID)
-    fs_group      = tostring(var.pgadmin4_config.fs_group)
-    listen_address = var.pgadmin4_config.listen_address
-    listen_port    = tostring(var.pgadmin4_config.listen_port)
-    server_mode    = tostring(var.pgadmin4_config.server_mode)
-    server_ip      = var.server_ip
-    mount          = var.pgadmin4_config.mount
-    port           = tostring(var.pgadmin4_config.port)
+    pgadmin4_email         = var.pgadmin4_config.email
+    pgadmin4_password      = var.pgadmin4_config.password
+    pgadmin4_uid           = tostring(var.pgadmin4_config.UID)
+    pgadmin4_gid           = tostring(var.pgadmin4_config.GID)
+    pgadmin4_fs_group      = tostring(var.pgadmin4_config.fs_group)
+    pgadmin4_listen_address = var.pgadmin4_config.listen_address
+    pgadmin4_listen_port    = tostring(var.pgadmin4_config.listen_port)
+    pgadmin4_server_mode    = tostring(var.pgadmin4_config.server_mode)
+    pgadmin4_server_ip      = var.server_ip
+    pgadmin4_mount          = var.pgadmin4_config.mount
+    pgadmin4_port           = tostring(var.pgadmin4_config.port)
   }
 
   type = "Opaque"
 }
 
 ################################################################################
-# pgAdmin4 Server Connections Secret
+# pgAdmin4 Server Connections Config
 ################################################################################
 
-resource "kubernetes_secret" "pgadmin4_servers" {
+resource "kubernetes_config_map" "pgadmin4_servers_config" {
   metadata {
-    name      = "pgadmin4-servers-secret"
+    name      = "pgadmin4-servers-config"
     namespace = kubernetes_namespace.pgsql.metadata[0].name
     labels = {
       app        = "pgadmin4"
@@ -241,47 +359,57 @@ resource "kubernetes_secret" "pgadmin4_servers" {
   }
 
   data = {
-    "servers.json" = jsonencode({
-      Servers = {
-        "1" = {
-          Name = "PostgreSQL-DEV"
-          Group = "Development"
-          Host = "dev-pgsql.${kubernetes_namespace.pgsql.metadata[0].name}.svc.cluster.local"
-          Port = var.pgsql_config.dev.port
-          MaintenanceDB = var.pgsql_config.dev.database
-          Username = var.pgsql_config.dev.user
-          Password = var.pgsql_config.dev.password
-          SSLMode = "prefer"
-        },
-        "2" = {
-          Name = "PostgreSQL-STG"
-          Group = "Staging"
-          Host = "stg-pgsql.${kubernetes_namespace.pgsql.metadata[0].name}.svc.cluster.local"
-          Port = var.pgsql_config.stg.port
-          MaintenanceDB = var.pgsql_config.stg.database
-          Username = var.pgsql_config.stg.user
-          Password = var.pgsql_config.stg.password
-          SSLMode = "prefer"
-        },
-        "3" = {
-          Name = "PostgreSQL-PROD"
-          Group = "Production"
-          Host = "prod-pgsql.${kubernetes_namespace.pgsql.metadata[0].name}.svc.cluster.local"
-          Port = var.pgsql_config.prod.port
-          MaintenanceDB = var.pgsql_config.prod.database
-          Username = var.pgsql_config.prod.user
-          Password = var.pgsql_config.prod.password
-          SSLMode = "prefer"
-        }
-      }
-    })
+    pgadmin4_dev_server_name = "PostgreSQL-DEV"
+    pgadmin4_dev_server_group = "Development"
+    pgadmin4_dev_server_host = "dev-pgsql.${kubernetes_namespace.pgsql.metadata[0].name}.svc.cluster.local"
+    pgadmin4_dev_server_port = tostring(var.pgsql_config.dev.port)
+    pgadmin4_dev_server_db = var.pgsql_config.dev.database
+    pgadmin4_dev_server_user = var.pgsql_config.dev.user
+    pgadmin4_dev_server_sslmode = "prefer"
+    
+    pgadmin4_stg_server_name = "PostgreSQL-STG"
+    pgadmin4_stg_server_group = "Staging"
+    pgadmin4_stg_server_host = "stg-pgsql.${kubernetes_namespace.pgsql.metadata[0].name}.svc.cluster.local"
+    pgadmin4_stg_server_port = tostring(var.pgsql_config.stg.port)
+    pgadmin4_stg_server_db = var.pgsql_config.stg.database
+    pgadmin4_stg_server_user = var.pgsql_config.stg.user
+    pgadmin4_stg_server_sslmode = "prefer"
+    
+    pgadmin4_prod_server_name = "PostgreSQL-PROD"
+    pgadmin4_prod_server_group = "Production"
+    pgadmin4_prod_server_host = "prod-pgsql.${kubernetes_namespace.pgsql.metadata[0].name}.svc.cluster.local"
+    pgadmin4_prod_server_port = tostring(var.pgsql_config.prod.port)
+    pgadmin4_prod_server_db = var.pgsql_config.prod.database
+    pgadmin4_prod_server_user = var.pgsql_config.prod.user
+    pgadmin4_prod_server_sslmode = "prefer"
+  }
+}
+
+################################################################################
+# pgAdmin4 Server Connections Secret for Passwords
+################################################################################
+
+resource "kubernetes_secret" "pgadmin4_servers_secrets" {
+  metadata {
+    name      = "pgadmin4-servers-secrets"
+    namespace = kubernetes_namespace.pgsql.metadata[0].name
+    labels = {
+      app        = "pgadmin4"
+      managed-by = "terraform"
+    }
+  }
+
+  data = {
+    pgadmin4_dev_server_password = var.pgsql_config.dev.password
+    pgadmin4_stg_server_password = var.pgsql_config.stg.password
+    pgadmin4_prod_server_password = var.pgsql_config.prod.password
   }
 
   type = "Opaque"
 }
 
 ################################################################################
-# Global Configuration Secret
+# Global Configuration 
 ################################################################################
 
 resource "kubernetes_config_map" "server_ip_config" {
@@ -294,7 +422,7 @@ resource "kubernetes_config_map" "server_ip_config" {
   }
 
   data = {
-    SERVER_IP = var.server_ip
+    server_ip = var.server_ip
   }
 }
 
